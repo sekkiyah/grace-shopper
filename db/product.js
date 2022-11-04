d5a168c7e4aeb7304d8fd58cba04e33a91cdf8a0
Francis Harbourt <nharbourt@gmail.com>
2022-11-04 19:36:57 -0400

    finished basic queries in product.js

--- db/product.js
+++ db/product.js
@@ -1,24 +1,83 @@
 const client = require('./client');
 
-async function createProduct({ title, description }) {
+async function createProduct({ name, description, price, inventory, thumbnailImage}) {
   try {
     const {
       rows: [product],
     } = await client.query(
       `
-      INSERT INTO products(title, description)
-      VALUES ($1, $2)
+      INSERT INTO products(name, description, price, inventory, "thumbnailImage")
+      VALUES ($1, $2, $3, $4, $5)
       RETURNING *;
     `,
-      [title, description]
+      [name, description, price, inventory, thumbnailImage]
     );
 
     return product;
-  } catch (err) {
+
+  } catch (error) {
     console.error('Error creating product');
-    console.error(err);
+    console.error(error);
   }
 }
+async function getProductById(id){
+  try {
+    const {rows: [product] } = await client.query(`
+    SELECT *
+    FROM products
+    WHERE id=$1;
+    `, [id]);
+
+    return product
+
+  } catch (error) {
+    console.error('Error getting product by id');
+    console.error(error)
+  }
+}
+async function updateProduct({ id, ...fields}){
+  const { update } = fields;
+  delete fields.tags;
+
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  try {
+    if(setString.length > 0){
+      await client.query(`
+      UPDATE products
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *;
+      `,
+      Object.values(fields))
+    };
+    if(update === undefined){
+      return await getProductById(id)
+    }
+
+  } catch (error) {
+    console.error('Error updating product');
+    console.error(error);
+  }
+}
+async function deleteProduct(id){
+  try {
+    const { rows: deletedProduct } = await client.query(`
+    DELETE FROM products
+    WHERE id=$1
+    RETURNING *;
+    `, [id]);
+    
+    return deletedProduct;
+
+  } catch (error) {
+    console.error('Error deleting product');
+    console.error(error)
+  }
+}
+
 
 module.exports = {
   createProduct,

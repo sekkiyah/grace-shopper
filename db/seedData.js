ec3393704ee6def09028b3645d0d1f5abe384ed1
RachaelRoyack <rachaelroyack@aol.com>
2022-11-05 10:59:27 -0400

    edited order_history & order_details tables per jira

--- db/seedData.js
+++ db/seedData.js
@@ -8,8 +8,8 @@ async function dropTables() {
 		await client.query(`
       DROP TABLE IF EXISTS promo_codes;
       DROP TABLE IF EXISTS user_wishlist;
-      DROP TABLE IF EXISTS order_history;
       DROP TABLE IF EXISTS order_details;
+      DROP TABLE IF EXISTS order_history;
       DROP TABLE IF EXISTS product_categories;
       DROP TABLE IF EXISTS categories;
       DROP TABLE IF EXISTS product_images;
@@ -58,31 +58,33 @@ async function createTables() {
       CREATE TABLE user_cart(
         "userId" INTEGER REFERENCES users(id),
         "productId" INTEGER REFERENCES products(id),
-        quantity INTEGER
+        quantity INTEGER NOT NULL
       );
       CREATE TABLE product_images(
         "productId" INTEGER REFERENCES products(id),
-        "imageURL" VARCHAR
+        "imageURL" VARCHAR NOT NULL
       );
       CREATE TABLE categories(
         id SERIAL PRIMARY KEY,
-        name VARCHAR UNIQUE
+        name VARCHAR UNIQUE NOT NULL
       );
       CREATE TABLE product_categories(
         "productId" INTEGER REFERENCES products(id),
         "categoryId" INTEGER REFERENCES categories(id)
       );
-      CREATE TABLE order_details(
-        id SERIAL PRIMARY KEY,
-        "productId" INTEGER REFERENCES products(id),
-        quantity INTEGER,
-        price DECIMAL
-      );
+
       CREATE TABLE order_history(
+        id SERIAL PRIMARY KEY,
         "userId" INTEGER REFERENCES users(id),
-        "orderId" INTEGER REFERENCES order_details(id),
-        status VARCHAR,
-        total DECIMAL
+        status VARCHAR NOT NULL,
+        total DECIMAL (255,2) NOT NULL
+      );
+
+      CREATE TABLE order_details(
+        "orderId" INTEGER REFERENCES order_history(id),
+        "productId" INTEGER REFERENCES products(id),
+        quantity INTEGER NO NULL,
+        price DECIMAL (255,2) NOT NULL
       );
       CREATE TABLE user_wishlist(
         "userId" INTEGER REFERENCES users(id),
@@ -90,8 +92,8 @@ async function createTables() {
       );
       CREATE TABLE promo_codes(
         "productId" INTEGER REFERENCES products(id),
-        code VARCHAR UNIQUE,
-        "flatDiscount" DECIMAL,
+        code VARCHAR UNIQUE NOT NULL,
+        "flatDiscount" DECIMAL (255,2),
         "percentDiscount" INTEGER
       );
     `);

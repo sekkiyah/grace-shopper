e611d639b78e475779964eb7e15b177ddbf871e6
sekkiyah <seth_hinshaw@hotmail.com>
2022-11-05 12:44:03 -0500

    fixed database errors & updated test data generation methods

--- db/tables/users.js
+++ db/tables/users.js
@@ -1,144 +1,152 @@
 const client = require('../client');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 const createUser = async user => {
-	try {
-		const SALT_COUNT = 10;
+  try {
+    const SALT_COUNT = 10;
+    const hashedPassword = await bcrypt.hash(user.password, SALT_COUNT);
 
-		const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
+    user.password = hashedPassword;
 
-		user.password = hashedPassword
+    const columnNames = Object.keys(user).join('", "');
+    const valueString = Object.keys(user)
+      .map((key, index) => `$${index + 1}`)
+      .join();
 
-		const columnNames = Object.keys(user).join('", "');
-		const valueString = Object.keys(user)
-			.map((key, index) => `$${index + 1}`)
-			.join();
-
-		const {
-			rows: [newUser],
-		} = await client.query(
-			`
+    const {
+      rows: [newUser],
+    } = await client.query(
+      `
       INSERT INTO users("${columnNames}")
       VALUES (${valueString})
       RETURNING *;`,
-			Object.values(user)
-		);
+      Object.values(user)
+    );
 
-		return newUser;
-	} catch (err) {
-		console.error(err);
-	}
+    return newUser;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 async function getUser({ username, password }) {
-	try {
-	  const User = await getUserByUsername(username);
-  
-	  const hashedPassword = User.password;
-	  const isValid = await bcrypt.compare(password, hashedPassword)
-  
-	  if (isValid) {
-	  const { rows: [user] } = await client.query(` 
-		SELECT * FROM users
-		WHERE password = '${hashedPassword}' AND username='${username}';
-	  `);
+  try {
+    const _user = await getUserByUsername(username);
 
-		delete user.password
+    const hashedPassword = _user.password;
+    const isValid = await bcrypt.compare(password, hashedPassword);
 
-	  return user;
+    if (isValid) {
+      const {
+        rows: [user],
+      } = await client.query(` 
+				SELECT * FROM users
+				WHERE password = '${hashedPassword}' AND username='${username}';`);
 
-	  } else {
-		console.log ('Password not correct')
-		return null;
-	  }
-	} catch (error) {
-	  console.log(error);
-	  throw error;
-	}
-  }
-
-  async function getUserById(userId) {
-	try {
-	  const { rows: [user] } = await client.query(` 
-		SELECT * FROM users
-		WHERE id = ${userId}
-	  `);
-	  if (!user) {
-		return null;
-	  }
-	
-	  delete user.password
-
-	  return user;
-	} catch (error) {
-	  console.log(error);
-	  throw error;
-	}
-  
-  }
-  
-  async function getUserByUsername(userName) {
-	try {
-	  const { rows: [user] } = await client.query(` 
-		SELECT * FROM users
-		WHERE username = '${userName}';
-	  `);
-	  if (!user) {
-		return null;
-	  }
-  
-	  return user;
-	} catch (error) {
-	  console.log(error);
-	  throw error;
-	}
-  }
-
-  async function updateUser(id, fields = {}) {
-    const updatedColumns = Object.keys(fields).map (
-      (key, index) => `"${key}"=$${ index + 1}`
-    ).join(', ');
-
-    if (updatedColumns.length === 0) {
-      return;
-    }
-
-    try {
-      const {rows: [ user ]} = await client.query (`
-      UPDATE users
-      SET ${ updatedColumns }
-      WHERE id=${ id }
-      RETURNING *;
-      `, Object.values(fields));
+      delete user.password;
 
       return user;
-    } catch (error) {
-		console.log ('Error updating User')
-      throw error;
+    } else {
+      console.log('Username or password incorrect');
+      throw 'Username or password incorrect';
     }
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
-  
-  async function deleteUser(id) {
-	try {
+}
 
-	  const {rows: [user]} = await client.query (`
-	  DELETE FROM users WHERE users.id = ${id}
-	  RETURNING *;`)
-  
-	  return user
-  
-	} catch (error) {
-	  console.log('Error deleting User')
-	  throw error;
-	}
-  
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(` 
+			SELECT * FROM users
+			WHERE id = ${userId}
+	  `);
+    if (!user) {
+      console.error('User not found');
+      throw 'User not found';
+    }
+
+    delete user.password;
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
+}
+
+async function getUserByUsername(userName) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(` 
+			SELECT * FROM users
+			WHERE username = '${userName}';
+	  `);
+
+    if (!user) {
+      console.error('User not found');
+      throw 'User not found';
+    }
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+async function updateUser(id, fields = {}) {
+  const updatedColumns = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(', ');
+
+  if (updatedColumns.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      UPDATE users
+      SET ${updatedColumns}
+      WHERE id=${id}
+      RETURNING *;`,
+      Object.values(fields)
+    );
+
+    return user;
+  } catch (error) {
+    console.log('Error updating User');
+    throw error;
+  }
+}
+
+async function deleteUser(id) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+	  	DELETE FROM users WHERE users.id = ${id}
+	 		RETURNING *;`);
+
+    return user;
+  } catch (error) {
+    console.log('Error deleting User');
+    throw error;
+  }
+}
 
 module.exports = {
-	createUser,
-	getUser,
-	getUserByUsername,
-	getUserById,
-	updateUser,
-	deleteUser
+  createUser,
+  getUser,
+  getUserByUsername,
+  getUserById,
+  updateUser,
+  deleteUser,
 };
--- db/testData.js
+++ db/testData.js
@@ -1,206 +1,170 @@
 const { faker, FakerError } = require('@faker-js/faker');
 
 const createFakeUser = async () => {
+  const fakeUser = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    isAdmin: faker.datatype.boolean(),
+    isBanned: faker.datatype.boolean(),
+    passwordResetRequired: faker.datatype.boolean(),
+  };
 
-	const fakeUser = {
-		firstName: faker.name.firstName(),
-		lastName: faker.name.lastName(),
-		email: faker.internet.email(),
-		username: faker.internet.userName(),
-		password: faker.internet.password(),
-		isAdmin: faker.datatype.boolean(),
-		isBanned: faker.datatype.boolean(),
-		passwordResetRequired: faker.datatype.boolean(),
-	};
-
-	return fakeUser;
+  return fakeUser;
 };
 const createFakeProduct = async () => {
+  const fakeProduct = {
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    price: faker.commerce.price(25, 1000, 2, '$'),
+    inventory: faker.datatype.number({ max: 100 }),
+  };
 
-	const fakeProduct = {
-		name: faker.commerce.productName(),
-		description: faker.commerce.productDescription(),
-		price: faker.commerce.price(25, 1000, 2, '$'),
-		inventory: faker.datatype.number({ max: 100 }),
-	};
-
-	return fakeProduct;
+  return fakeProduct;
 };
 const createFakeProductReview = async () => {
+  const fakeProductReview = {
+    rating: faker.datatype.number({ max: 5 }),
+    title: faker.random.words(3),
+    content: faker.random.words(30),
+  };
 
-	const fakeProductReview = {
-		rating: faker.datatype.number({ max: 5 }),
-		title: faker.random.words(3),
-		content: faker.random.words(30),
-	};
-
-	return fakeProductReview;
+  return fakeProductReview;
 };
 const createFakeCartItem = async () => {
+  const fakeCartItem = {
+    quantity: faker.datatype.number({ max: 10 }),
+  };
 
-	const fakeCartItem = {
-		quantity: faker.datatype.number({ max: 10 }),
-	};
-
-	return fakeCartItem;
+  return fakeCartItem;
 };
 
 const createFakeProductImage = async () => {
-	
-	fakeImage = {
-		imageURL: faker.image.imageUrl('cat', true),
-	};
+  fakeImage = {
+    imageURL: faker.image.imageUrl('cat', true),
+  };
 
-	return fakeImage;
+  return fakeImage;
 };
 
 const createFakeCategory = async () => {
+  fakeCategory = {
+    name: faker.commerce.department(),
+  };
 
-	fakeCategory = {
-		name: faker.commerce.department(),
-	};
-
-	return fakeCategory;
+  return fakeCategory;
 };
 
 const createFakeOrderHistory = async () => {
+  fakeOrderHistory = {
+    status: faker.helpers.fake('Status: Complete'),
+    total: faker.commerce.price(25, 1000, 2, '$'),
+  };
 
-	fakeOrderHistory = {
-		status: faker.helpers.fake('Status: Complete'),
-		total: faker.commerce.price(25, 1000, 2, '$'),
-	};
-
-	return fakeOrderHistory;
+  return fakeOrderHistory;
 };
 
 const createFakeOrderDetail = async () => {
+  fakeOrderDetail = {
+    quantity: faker.datatype.number({ max: 10 }),
+    price: faker.commerce.price(25, 1000, 2, '$'),
+  };
 
-	fakeOrderDetail = {
-		quantity: faker.datatype.number({ max: 10 }),
-		price: faker.commerce.price(25, 1000, 2, '$'),
-	};
-
-	return fakeOrderDetail;
+  return fakeOrderDetail;
 };
 
 const createFakePromoCodes = async () => {
-	
-	fakePromoCode = {
-		code: faker.datatype.uuid(),
-		flatDiscount: faker.commerce.price(5, 25, 2, '$'),
-		percentDiscount: faker.datatype.float({ max: 100 }),
-	};
+  fakePromoCode = {
+    code: faker.datatype.uuid(),
+    flatDiscount: faker.commerce.price(5, 25, 2, '$'),
+    percentDiscount: faker.datatype.float({ max: 100 }),
+  };
 
-	return fakePromoCode;
+  return fakePromoCode;
 };
 
-const generateUsers = async numberOfUsers => {
-	const users = [];
-	for (let i = 0; i < numberOfUsers; i++) {
-		users.push(await createFakeUser());
-	}
-	return users;
+const generateUsers = async (numberOfUsers = 1) => {
+  const users = [];
+  for (let i = 0; i < numberOfUsers; i++) {
+    users.push(await createFakeUser());
+  }
+  return users;
 };
 
-const generateProducts = async (numberOfProducts) => {
-	if(!numberOfProducts){
-		numberOfProducts = 1;
-	}
-	const products = [];
-	for(let i = 0; i < numberOfProducts; i++){
-		products.push(await createFakeProduct());
-	}
-	return products;
-}
+const generateProducts = async (numberOfProducts = 1) => {
+  const products = [];
+  for (let i = 0; i < numberOfProducts; i++) {
+    products.push(await createFakeProduct());
+  }
+  return products;
+};
 
-const generateFakeProductReviews = async (numberOfReviews) => {
-	if(!numberOfReviews){
-		numberOfReviews = 1;
-	}
-	const reviews = [];
-	for(let i = 0; i < numberOfReviews; i++){
-		reviews.push(await createFakeProductReview());
-	}
-	return reviews;
-}
+const generateFakeProductReviews = async (numberOfReviews = 1) => {
+  const reviews = [];
+  for (let i = 0; i < numberOfReviews; i++) {
+    reviews.push(await createFakeProductReview());
+  }
+  return reviews;
+};
 
-const generateFakeCartItems = async (numberOfItems) => {
-	if(!numberOfItems){
-		numberOfItems = 1;
-	}
-	const items = [];
-	for(let i = 0; i < numberOfItems; i++){
-		items.push(await createFakeCartItem());
-	}
-	return items;
-}
+const generateFakeCartItems = async (numberOfItems = 1) => {
+  const items = [];
+  for (let i = 0; i < numberOfItems; i++) {
+    items.push(await createFakeCartItem());
+  }
+  return items;
+};
 
-const generateProductImages = async (numberOfImages) => {
-	if(!numberOfImages){
-		numberOfImages = 1;
-	}
-	const images = [];
-	for(let i = 0; i < numberOfImages; i++){
-		images.push(await createFakeProductImage());
-	}
-	return images;
-}
+const generateProductImages = async (numberOfImages = 1) => {
+  const images = [];
+  for (let i = 0; i < numberOfImages; i++) {
+    images.push(await createFakeProductImage());
+  }
+  return images;
+};
 
-const generateFakeCategories = async (numberOfCategories) => {
-	if(!numberOfCategories){
-		numberOfCategories = 1;
-	}
-	const categories = [];
-	for(let i = 0; i < numberOfCategories; i++){
-		categories.push(await createFakeCategory());
-	}
-	return categories;
-}
+const generateFakeCategories = async (numberOfCategories = 1) => {
+  const categories = [];
+  for (let i = 0; i < numberOfCategories; i++) {
+    categories.push(await createFakeCategory());
+  }
+  return categories;
+};
 
-const generateFakeOrderHistories = async (numberOfOrderHistories) => {
-	if(!numberOfOrderHistories){
-		numberOfOrderHistories = 1;
-	}
-	const orderHistories = [];
-	for(let i = 0; i < numberOfOrderHistories; i++){
-		orderHistories.push(await createFakeOrderHistory());
-	}
-	return orderHistories;
-}
+const generateFakeOrderHistories = async (numberOfOrderHistories = 1) => {
+  const orderHistories = [];
+  for (let i = 0; i < numberOfOrderHistories; i++) {
+    orderHistories.push(await createFakeOrderHistory());
+  }
+  return orderHistories;
+};
 
-const generateFakeOrderDetails = async (numberOfOrderDetails) => {
-	if(!numberOfOrderDetails){
-		numberOfOrderDetails = 1;
-	}
-	const orderDetails = [];
-	for(let i = 0; i < numberOfOrderDetails; i++){
-		orderDetails.push(await createFakeOrderDetail());
-	}
-	return orderDetails;
-}
+const generateFakeOrderDetails = async (numberOfOrderDetails = 1) => {
+  const orderDetails = [];
+  for (let i = 0; i < numberOfOrderDetails; i++) {
+    orderDetails.push(await createFakeOrderDetail());
+  }
+  return orderDetails;
+};
 
-const generateFakePromoCodes = async (numberOfPromoCodes) => {
-	if(!numberOfPromoCodes){
-		numberOfPromoCodes = 1;
-	}
-	const promoCodes = [];
-	for(let i = 0; i < numberOfPromoCodes; i++){
-		promoCodes.push(await createFakePromoCodes());
-	}
-	return promoCodes;
-}
-
-
-
+const generateFakePromoCodes = async (numberOfPromoCodes = 1) => {
+  const promoCodes = [];
+  for (let i = 0; i < numberOfPromoCodes; i++) {
+    promoCodes.push(await createFakePromoCodes());
+  }
+  return promoCodes;
+};
 
 module.exports = {
-	generateUsers,
-	generateProducts,
-	generateFakeCartItems,
-	generateFakeCategories,
-	generateFakeOrderDetails,
-	generateFakeProductReviews,
-	generateProductImages,
-	generateFakeOrderHistories,
-	generateFakePromoCodes
+  generateUsers,
+  generateProducts,
+  generateFakeCartItems,
+  generateFakeCategories,
+  generateFakeOrderDetails,
+  generateFakeProductReviews,
+  generateProductImages,
+  generateFakeOrderHistories,
+  generateFakePromoCodes,
 };

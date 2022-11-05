b54ee7e9eda41f1b2059689fe535542199f82c2f
RachaelRoyack <rachaelroyack@aol.com>
2022-11-05 11:48:49 -0400

    added bcrypt in createUsers & wrote getUser/getUserById/getUserByUsername

--- db/tables/users.js
+++ db/tables/users.js
@@ -1,7 +1,14 @@
 const client = require('../client');
+const bcrypt = require('bcrypt')
 
 const createUser = async user => {
 	try {
+		const SALT_COUNT = 10;
+
+		const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
+
+		user.password = hashedPassword
+
 		const columnNames = Object.keys(user).join('", "');
 		const valueString = Object.keys(user)
 			.map((key, index) => `$${index + 1}`)
@@ -23,6 +30,75 @@ const createUser = async user => {
 	}
 };
 
+async function getUser({ username, password }) {
+	try {
+	  const User = await getUserByUsername(username);
+  
+	  const hashedPassword = User.password;
+	  const isValid = await bcrypt.compare(password, hashedPassword)
+  
+  
+	  if (isValid) {
+	  const { rows: [user] } = await client.query(` 
+		SELECT * FROM users
+		WHERE password = '${hashedPassword}' AND username='${username}';
+	  `);
+
+		delete user.password
+
+	  return user;
+
+	  } else {
+		console.log ('Password not correct')
+		return null;
+	  }
+	} catch (error) {
+	  console.log(error);
+	  throw error;
+	}
+  }
+
+  async function getUserById(userId) {
+	try {
+	  const { rows: [user] } = await client.query(` 
+		SELECT * FROM users
+		WHERE id = ${userId}
+	  `);
+	  if (!user) {
+		return null;
+	  }
+	
+	  delete user.password
+
+	  return user;
+	} catch (error) {
+	  console.log(error);
+	  throw error;
+	}
+  
+  }
+  
+  async function getUserByUsername(userName) {
+  
+	try {
+	  const { rows: [user] } = await client.query(` 
+		SELECT * FROM users
+		WHERE username = '${userName}';
+	  `);
+	  if (!user) {
+		return null;
+	  }
+  
+	  return user;
+	} catch (error) {
+	  console.log(error);
+	  throw error;
+	}
+  }
+
 module.exports = {
 	createUser,
+	getUser,
+	getUserByUsername,
+	getUserById
 };

18c7116c264fd412b9732dd14c86aba4684356ac
alexmenke <alexrm2017@gmail.com>
2022-11-05 11:17:10 -0500

    built queries for: categories, product_categories, product_images, user_cart

--- db/tables/product_categories.js
+++ db/tables/product_categories.js
@@ -1,2 +1,80 @@
 const client = require('../client');
-module.exports = {};
+
+async function createProductCategory ({productId, categoryId}) {
+    try {
+        const { rows: [productCategory] } = await client.query(`
+            INSERT INTO product_categories("productId", "categoryId")
+            VALUES ($1, $2)
+            RETURNING *;
+            `, [productId, categoryId])
+
+            return productCategory;
+
+    } catch (error) {
+        console.error("Error creating product category");
+        console.error(error);
+    }
+}
+
+async function getProductCategoryById(id){
+    try {
+        const {rows: [productCategory]} = await client.query(`
+        SELECT *
+        FROM product_categories
+        WHERE id=$1;
+        `, [id]);
+
+        return productCategory;
+
+    } catch (error) {
+        console.error("Error getting product category by id");
+        console.error(error);
+    }
+}
+
+async function updateProductCategory ({id, ...fields}) {
+    const { update } = fields;
+
+    const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+    try {
+        if(setString.length > 0){
+            await client.query(`
+            UPDATE product_categories
+            SET ${setString}
+            WHERE id=${id}
+            RETURNING *;
+            `, Object.values(fields));
+        }
+        if(update === undefined){
+            return await getProductCategoryById(id);
+        }
+    } catch (error) {
+        console.error("Error updating product category");
+        console.error(error);
+    }
+}
+
+async function deleteProductCategory (id) {
+    try {
+        const { rows: deletedProductCategory } = await client.query(`
+        DELETE FROM product_categories
+        WHERE id=$1
+        RETURNING *;
+    `, [id]);
+
+    return deletedProductCategory;
+
+    } catch (error) {
+        console.error("Error deleting product category");
+        console.error(error);
+    }
+}
+module.exports = {
+    createProductCategory,
+    getProductCategoryById,
+    updateProductCategory,
+    deleteProductCategory
+};
--- db/tables/user_cart.js
+++ db/tables/user_cart.js
@@ -1,2 +1,81 @@
 const client = require('../client');
-module.exports = {};
+
+async function createUserCart({ userId, productId, quantity }) {
+    try {
+        const { rows: [user_cart] } = await client.query(`
+            INSERT INTO user_cart("userId", "productId", quantity)
+            VALUES ($1, $2, $3)
+            RETURNING *;
+        `, [userId, productId, quantity]);
+
+        return user_cart;
+
+    } catch (error) {
+        console.error('Error creating user cart');
+        console.error(error);
+    }
+};
+
+async function getUserCartById(id){
+    try {
+        const {rows: [user_cart]} = await client.query(`
+        SELECT *
+        FROM user_cart
+        WHERE id=$1;
+        `, [id]);
+
+        return user_cart;
+
+    } catch (error) {
+        console.error("Error getting user cart by id");
+        console.error(error);
+    }
+}
+
+async function updateUserCart({id, ...fields}) {
+    const { update } = fields;
+
+    const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+    try {
+        if(setString.length > 0){
+            await client.query(`
+            UPDATE user_cart
+            SET ${setString}
+            WHERE id=${id}
+            RETURNING *;
+            `, Object.values(fields));
+        }
+        if(update === undefined){
+            return await getUserCartById(id);
+        }
+    } catch (error) {
+        console.error('Error updating user cart');
+        console.error(error);
+    }
+};
+
+async function deleteUserCart(id) {
+    try {
+        const { rows: deletedUserCart } = await client.query(`
+            DELETE FROM user_cart
+            WHERE id=$1
+            RETURNING *;
+        `, [id]);
+
+        return deletedUserCart;
+
+    } catch (error) {
+        console.error('Error deleting user cart');
+        console.error(error);
+    }
+};
+
+module.exports = {
+    createUserCart,
+    getUserCartById,
+    updateUserCart,
+    deleteUserCart
+};

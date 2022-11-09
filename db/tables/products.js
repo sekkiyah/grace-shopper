198439439196c9697f0ea635f9e65eec7deafc47
sekkiyah <seth_hinshaw@hotmail.com>
2022-11-08 22:14:05 -0600

    fixed products db create function

--- db/tables/products.js
+++ db/tables/products.js
@@ -1,90 +1,95 @@
 const client = require('../client');
 
-async function createProduct({ id, name, description, price, inventory, thumbnailImage}) {
+async function createProduct({ name, description, price, inventory, thumbnailImage }) {
   try {
     const {
       rows: [product],
     } = await client.query(
       `
-      INSERT INTO products(id, name, description, price, inventory, "thumbnailImage")
-      VALUES ($1, $2, $3, $4, $5, $6)
+      INSERT INTO products(name, description, price, inventory, "thumbnailImage")
+      VALUES ($1, $2, $3, $4, $5)
       RETURNING *;
     `,
-      [id, name, description, price, inventory, thumbnailImage]
+      [name, description, price, inventory, thumbnailImage]
     );
 
     return product;
-
   } catch (error) {
     console.error('Error creating product');
     console.error(error);
   }
-}; 
+}
 
-async function getProductById(id){
+async function getProductById(id) {
   try {
-    const {rows: [product] } = await client.query(`
+    const {
+      rows: [product],
+    } = await client.query(
+      `
     SELECT *
     FROM products
     WHERE id=$1;
-    `, [id]);
+    `,
+      [id]
+    );
 
-    return product
-
+    return product;
   } catch (error) {
     console.error('Error getting product by id');
-    console.error(error)
+    console.error(error);
   }
-};
+}
 
-async function updateProduct({ id, ...fields}){
+async function updateProduct({ id, ...fields }) {
   const { update } = fields;
   delete fields.tags;
 
   const setString = Object.keys(fields)
     .map((key, index) => `"${key}"=$${index + 1}`)
-    .join(", ");
+    .join(', ');
 
   try {
-    if(setString.length > 0){
-      await client.query(`
+    if (setString.length > 0) {
+      await client.query(
+        `
       UPDATE products
       SET ${setString}
       WHERE id=${id}
       RETURNING *;
       `,
-      Object.values(fields))
-    };
-    if(update === undefined){
-      return await getProductById(id)
+        Object.values(fields)
+      );
     }
-
+    if (update === undefined) {
+      return await getProductById(id);
+    }
   } catch (error) {
     console.error('Error updating product');
     console.error(error);
   }
-};
+}
 
-async function deleteProduct(id){
+async function deleteProduct(id) {
   try {
-    const { rows: deletedProduct } = await client.query(`
+    const { rows: deletedProduct } = await client.query(
+      `
     DELETE FROM products
     WHERE id=$1
     RETURNING *;
-    `, [id]);
-    
-    return deletedProduct;
+    `,
+      [id]
+    );
 
+    return deletedProduct;
   } catch (error) {
     console.error('Error deleting product');
-    console.error(error)
+    console.error(error);
   }
 }
-
 
 module.exports = {
   createProduct,
   getProductById,
   updateProduct,
-  deleteProduct
+  deleteProduct,
 };

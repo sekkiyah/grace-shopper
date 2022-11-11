2b1c4483b70e71e78a0cb139d86c6d85647ecec5
sekkiyah <seth_hinshaw@hotmail.com>
2022-11-10 23:23:18 -0600

    revamped database seeding for dynamic functions

--- db/tables/categories.js
+++ db/tables/categories.js
@@ -1,80 +1,94 @@
 const client = require('../client');
 
-async function createCategory({ id, name }) {
-    try {
-        const { rows: [category] } = await client.query(`
-            INSERT INTO categories(id, name)
-            VALUES ($1, $2)
-            RETURNING *;
-            `, [id, name]);
+async function createCategory({ name }) {
+  try {
+    const {
+      rows: [category],
+    } = await client.query(
+      `
+        INSERT INTO categories(name)
+        VALUES ($1)
+        ON CONFLICT (name) DO NOTHING
+        RETURNING *;`,
+      [name]
+    );
 
-            return category;
-
-    } catch (error) {
-        console.error('Error creating category');
-        console.error(error);
-    }
-};
+    return category;
+  } catch (error) {
+    console.error('Error creating category');
+    console.error(error);
+  }
+}
 
 async function getCategoryById(id) {
-    try {
-        const {rows: [category]} = await client.query(`
+  try {
+    const {
+      rows: [category],
+    } = await client.query(
+      `
         SELECT *
         FROM category
         WHERE id=$1;
-        `, [id]);
+        `,
+      [id]
+    );
 
-        return category;
-    } catch (error) {
-        console.error("Error getting category by id");
-        console.error(error);
-    }
-};
+    return category;
+  } catch (error) {
+    console.error('Error getting category by id');
+    console.error(error);
+  }
+}
 
-async function updateCategory({id, ...fields}) {
-    const { update } = fields;
+async function updateCategory({ id, ...fields }) {
+  const { update } = fields;
 
-    const setString = Object.keys(fields)
+  const setString = Object.keys(fields)
     .map((key, index) => `"${key}"=$${index + 1}`)
-    .join(", ");
+    .join(', ');
 
-    try {
-        if(setString.length > 0){
-            await client.query(`
+  try {
+    if (setString.length > 0) {
+      await client.query(
+        `
             UPDATE categories
             SET ${setString}
             WHERE id=${id}
             RETURNING *;
-            `, Object.values(fields));
-        }
-        if(update === undefined){
-            return await getCategoryById(id);
-        }
-    } catch (error) {
-        console.error('Error updating category');
-        console.error(error);
+            `,
+        Object.values(fields)
+      );
     }
-};
+    if (update === undefined) {
+      return await getCategoryById(id);
+    }
+  } catch (error) {
+    console.error('Error updating category');
+    console.error(error);
+  }
+}
 
 async function deleteCategory(id) {
-    try {
-        const { rows: deletedCategory } = await client.query(`
+  try {
+    const { rows: deletedCategory } = await client.query(
+      `
             DELETE FROM categories
             WHERE id=$1
             RETURNING *;
-        `, [id]);
+        `,
+      [id]
+    );
 
-        return deletedCategory;
-
-    } catch (error) {
-        console.error('Error deleting category');
-        console.error(error);
-    }
-};
+    return deletedCategory;
+  } catch (error) {
+    console.error('Error deleting category');
+    console.error(error);
+  }
+}
 
 module.exports = {
-    createCategory,
-    getCategoryById,
-    updateCategory,
-    deleteCategory
+  createCategory,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
 };

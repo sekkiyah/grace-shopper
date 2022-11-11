259d19c4359eb939241e183754e48aae5cfee0a2
RachaelRoyack <rachaelroyack@aol.com>
2022-11-10 21:06:56 -0500

    users db added functions & fixed createOrderHistory

--- db/tables/order_history.js
+++ db/tables/order_history.js
@@ -3,8 +3,8 @@ const client = require('../client');
 async function createOrderHistory ({id, userId, status, total, dateOrdered}) {
     try {
         const { rows: [order_history] } = await client.query(`
-            INSERT INTO order_history(id, "userId", status, total, "dateOrdered")
-            VALUES ($1, $2, $3, $4, $5)
+            INSERT INTO order_history("userId", status, total, "dateOrdered")
+            VALUES ($1, $2, $3, $4)
             RETURNING *;
         `, [id, userId, status, total, dateOrdered]);
 

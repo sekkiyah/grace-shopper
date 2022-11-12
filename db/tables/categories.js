aa6d1d9ab75a930773060ce03fad508c232ae528
alexmenke <alexrm2017@gmail.com>
2022-11-12 11:09:22 -0600

    conflict resolved

--- db/tables/categories.js
+++ db/tables/categories.js
@@ -1,9 +1,5 @@
 const client = require('../client');
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e86eaf7fcd5bed7dfef7997266c842f15f1dc9df
 async function createCategory({name}) {
     try {
         const { rows: [category] } = await client.query(`

1a23703ab772637d8951f6c9eb79d702f3aa4a95
RachaelRoyack <rachaelroyack@aol.com>
2022-11-12 12:22:22 -0500

    repulled seeData from seth branch

--- db/seedData.js
+++ db/seedData.js
@@ -1,6 +1,19 @@
 const client = require('./client');
-const { createUser, createProduct, createProductImage, createProductReview } = require('./tables');
-const { generateUsers, generateProducts, generateProductImages, generateFakeProductReviews } = require('./testData');
+const {
+  createUser,
+  createProduct,
+  createProductImage,
+  createProductReview,
+  createCategory,
+  createProductCategory,
+} = require('./tables');
+const {
+  generateUsers,
+  generateProducts,
+  generateProductImages,
+  generateFakeProductReviews,
+  generateFakeCategories,
+} = require('./testData');
 
 async function dropTables() {
   try {
@@ -109,14 +122,15 @@ async function createTables() {
 }
 
 async function createInitialUsers() {
-  console.log('Starting to create users...');
   try {
-    const usersToCreate = await generateUsers(10);
+    console.log('Starting to create users...');
+
+    const usersToCreate = await generateUsers(50);
     const users = await Promise.all(usersToCreate.map(user => createUser(user)));
 
     console.log('Users created:');
     // console.log(users);
-    console.log('Finished creating users!');
+    return users;
   } catch (error) {
     console.error('Error creating users!');
     throw error;
@@ -127,20 +141,12 @@ async function createInitialProducts() {
   try {
     console.log('Starting to create products...');
 
-    const productsToCreate = await generateProducts(10);
+    const productsToCreate = await generateProducts(75);
     const products = await Promise.all(productsToCreate.map(product => createProduct(product)));
     console.log('Products created:');
-    console.log(products);
+    // console.log(products);
 
-    await createInitialProductImages(products);
-    console.log('Product Images created:');
-    // console.log(productImages);
-
-    const reviews = await createInitialProductReviews(products);
-    console.log('Product Reviews created:');
-    console.log(reviews);
-
-    console.log('Finished creating products');
+    return products;
   } catch (error) {
     console.error('Error creating products');
     throw error;
@@ -149,6 +155,8 @@ async function createInitialProducts() {
 
 async function createInitialProductImages(products) {
   try {
+    console.log('Starting to create product images...');
+
     for (let x = 0; x < products.length; x++) {
       let randomNum = Math.ceil(Math.random() * 2);
       let productImages = await generateProductImages(randomNum, products[x].id);
@@ -156,25 +164,107 @@ async function createInitialProductImages(products) {
         await Promise.all(productImages.map(imageObj => createProductImage(imageObj)));
       }
     }
+
+    console.log('Product Images created');
   } catch (err) {
     console.error('Error creating product images');
     throw err;
   }
 }
 
-async function createInitialProductReviews(products) {
+async function createInitialProductReviews(users, products) {
   try {
-    const productReviews = await generateFakeProductReviews(10);
-    const result = await Promise.all(
-      productReviews.map(review => {
-        review.productId = Math.ceil(Math.random() * products.length);
-        review.userId = Math.ceil(Math.random() * 10);
+    console.log('Starting to create product reviews...');
+
+    const productReviews = await generateFakeProductReviews(40);
+    const matrix = await buildUniqueIdMatrix(users, products, productReviews.length);
+
+    const reviews = await Promise.all(
+      productReviews.map((review, index) => {
+        review.userId = matrix[index][0];
+        review.productId = matrix[index][1];
         return createProductReview(review);
       })
     );
-    return result;
+
+    console.log('Product Reviews created');
+    // console.log(reviews);
+    return reviews;
   } catch (err) {
-    console.error('Error creating product images');
+    console.error('Error creating product reviews');
+    throw err;
+  }
+}
+
+async function createInitialCategories() {
+  try {
+    console.log('Starting to create categories...');
+
+    const categoriesToCreate = await generateFakeCategories(10);
+    const result = await Promise.all(categoriesToCreate.map(category => createCategory(category)));
+    const categories = result.filter(category => category); //Remove undefined values
+
+    console.log('Categories created:');
+    console.log(categories);
+
+    return categories;
+  } catch (err) {
+    console.error('Error creating categories');
+    throw err;
+  }
+}
+
+async function createInitialProductCategories(products, categories) {
+  try {
+    const matrix = await buildUniqueIdMatrix(products, categories, 40); //Assign to 40 products
+
+    await Promise.all(
+      matrix.map(row =>
+        createProductCategory({
+          productId: row[0],
+          categoryId: row[1],
+        })
+      )
+    );
+
+    console.log('Product Categories created');
+  } catch (err) {
+    console.error('Error creating product categories');
+    throw err;
+  }
+}
+
+async function buildUniqueIdMatrix(array1, array2, loopCount) {
+  try {
+    let loops = 0;
+    let matrix = [];
+
+    while (loops < loopCount) {
+      let random1 = Math.ceil(Math.random() * array1.length - 1);
+      let random2 = Math.ceil(Math.random() * array2.length - 1);
+      let id1 = array1[random1].id;
+      let id2 = array2[random2].id;
+
+      let duplicateFound = false;
+
+      for (let n = 0; n < matrix.length; n++) {
+        if (matrix[n][0] === id1 && matrix[n][1] === id2) {
+          duplicateFound = true;
+          break;
+        }
+      }
+
+      if (duplicateFound) {
+        continue;
+      } else {
+        matrix.push([id1, id2]);
+        loops++;
+      }
+    }
+
+    return matrix;
+  } catch (err) {
+    console.error('Error during unique matrix build');
     throw err;
   }
 }
@@ -183,8 +273,12 @@ async function rebuildDB() {
   try {
     await dropTables();
     await createTables();
-    await createInitialUsers();
-    await createInitialProducts();
+    const allUsers = await createInitialUsers();
+    const allProducts = await createInitialProducts();
+    await createInitialProductImages(allProducts);
+    await createInitialProductReviews(allUsers, allProducts);
+    const allCategories = await createInitialCategories();
+    await createInitialProductCategories(allProducts, allCategories);
   } catch (err) {
     console.error('Error during rebuildDB');
     throw err;

2ca57aec99913e04acaa90f3261f0f591b2a7728
sekkiyah <seth_hinshaw@hotmail.com>
2022-11-14 20:31:54 -0600

    built initial react files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+
+const App = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Routes>{/* <Route path='/' element={}></Route> */}</Routes>
+    </>
+  );
+};
+
+export default App;

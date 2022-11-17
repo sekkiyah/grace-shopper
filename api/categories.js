3a28e974cd353fa7377f0c6953c42a2bbeb39b6e
alexmenke <alexrm2017@gmail.com>
2022-11-17 16:41:22 -0600

    built out api endpoints for categories and order_history

--- api/categories.js
+++ api/categories.js
@@ -1,8 +1,84 @@
 const express = require('express');
-const router = express.Router();
+const categoriesRouter = express.Router();
+// const { requireAdmin } = require("./utils");
+const {
+  createCategory,
+  getAllCategories,
+  getCategoryById,
+  updateCategory,
+  deleteCategory
+} = require('../db/tables/categories');
 
-router.get('/', (req, res, next) => {
+categoriesRouter.get('/', (req, res, next) => {
   res.send('categories API in progress');
 });
 
-module.exports = router;
+categoriesRouter.use((req, res, next) => {
+  console.log('A request is being made to /categories')
+});
+
+//GET /api/categories
+categoriesRouter.get('/', async (req, res, next) => {
+  try {
+    const allCategories = await getAllCategories();
+
+    res.send({
+      allCategories
+    })
+
+  } catch (error) {
+    res.send(error)
+  }
+});
+
+//POST /api/categories
+categoriesRouter.post('/', requireAdmin, async (req, res, next) => {
+  const { name } = req.body;
+
+  const categoriesData = {};
+  try {
+    categoriesData.name = name;
+    const category = await createCategory(categoriesData);
+
+    res.send(category)
+  } catch (error) {
+    res.send(error)
+  }
+});
+
+//PATCH /api/categories/categoryId
+categoriesRouter.patch('/:categoryId', requireAdmin, async (req, res, next) => {
+  const { categoryId } = req.params.categoryId;
+  const { name } = req.body;
+
+  const updateFields = {};
+
+  if (name) {
+    updateFields.name = name;
+  }
+
+  try {
+    const originalCategory = await getCategoryById(categoryId);
+    const updatedCategory = await updateCategory(originalCategory, ...updateFields);
+
+    res.send(updatedCategory)
+  } catch (error) {
+    res.send(error)
+  }
+});
+
+//DELETE /api/category/categoryId
+categoriesRouter.delete('/:categoryId', requireAdmin, async (req, res, next) => {
+  const category = await getCategoryById(req.params.categoryId);
+
+  const deletedCategory = await deleteCategory(category.categoryId)
+  try {
+    if (deletedCategory) {
+      res.send(deletedCategory)
+    }
+  } catch (error) {
+    res.send(error)
+  }
+});
+
+module.exports = categoriesRouter;
--- api/order_history.js
+++ api/order_history.js
@@ -1,8 +1,67 @@
 const express = require('express');
-const router = express.Router();
+const orderHistoryRouter = express.Router();
+// const { requireAdmin } = require("./utils");
+const {
+  getAllOrderHistories,
+  getOrderHistoryByUserId,
+  updateOrderHistory
+} = require('../db/tables/order_history');
 
-router.get('/', (req, res, next) => {
+orderHistoryRouter.get('/', (req, res, next) => {
   res.send('order_history API in progress');
 });
 
-module.exports = router;
+orderHistoryRouter.use((req, res, next) => {
+  console.log('A request is being made to /order_history')
+});
+
+//GET /api/order_history
+orderHistoryRouter.get('/', async (req, res, next) => {
+  try {
+    const allOrderHistories = await getAllOrderHistories();
+
+    res.send({
+      allOrderHistories
+    })
+
+  } catch (error) {
+    res.send(error)
+  }
+});
+
+//GET /api/order_history/:orderHistoryId
+orderHistoryRouter.get('/:orderHistoryId', requireAdmin, (req, res, next) => {
+  try {
+    const userOrderHistory = await getOrderHistoryByUserId();
+
+    res.send({
+      userOrderHistory
+    })
+
+  } catch (error) {
+    res.send(error)
+  }
+});
+
+//PATCH /api/order_history/:orderHistoryId
+orderHistoryRouter.patch('/:orderHistoryId', requireAdmin, (req, res, next) => {
+  const { userId } = req.params;
+  const { status } = req.body;
+
+  const updateFields = {};
+
+  if (status) {
+    updateFields.status = status;
+  }
+
+  try {
+    const updatedOrderHistory = await updateOrderHistory({ id: userId, ...updateFields });
+
+    res.send(updatedOrderHistory)
+
+  } catch (error) {
+    res.send(error)
+  }
+});
+
+module.exports = orderHistoryRouter;

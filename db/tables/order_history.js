8b396ec58ae059d6a78cbf77b4087a50ae347fa2
sekkiyah <seth_hinshaw@hotmail.com>
2022-11-16 22:28:49 -0600

    updated delete queries in other tables for deletion by user id data dependencies

--- db/tables/order_history.js
+++ db/tables/order_history.js
@@ -1,98 +1,139 @@
 const client = require('../client');
+const { deleteOrderDetailsByOrderId } = require('./order_details');
 
-async function createOrderHistory ({userId, status, total, dateOrdered}) {
-    try {
-        const { rows: [order_history] } = await client.query(`
+async function createOrderHistory({ userId, status, total, dateOrdered }) {
+  try {
+    const {
+      rows: [order_history],
+    } = await client.query(
+      `
             INSERT INTO order_history("userId", status, total, "dateOrdered")
             VALUES ($1, $2, $3, $4)
             RETURNING *;
-        `, [userId, status, total, dateOrdered]);
+        `,
+      [userId, status, total, dateOrdered]
+    );
 
-        return order_history;
+    return order_history;
+  } catch (error) {
+    console.error('Error creating order history');
+    throw error;
+  }
+}
 
-    } catch (error) {
-        console.error('Error creating order history');
-        throw error;
-    }
-};
-
-async function getOrderHistoryByUserId(userId){
-    try {
-        const {rows: [order_history]} = await client.query(`
+async function getOrderHistoryByUserId(userId) {
+  try {
+    const {
+      rows: [order_history],
+    } = await client.query(
+      `
         SELECT *
         FROM order_history
         WHERE "userId"=$1;
-        `, [userId]);
+        `,
+      [userId]
+    );
 
-        return order_history;
+    return order_history;
+  } catch (error) {
+    console.error("Error getting order history by 'userId'");
+    throw error;
+  }
+}
 
-    } catch (error) {
-        console.error("Error getting order history by 'userId'");
-        throw error;
-    }
-};
-
-async function getAllOrderHistories (id) {
-    try {
-        const {rows: [order_histories]} = await client.query(`
+async function getAllOrderHistories(id) {
+  try {
+    const {
+      rows: [order_histories],
+    } = await client.query(
+      `
         SELECT *
         FROM order_history
         WHERE id=$1;
-        `, [id]);
+        `,
+      [id]
+    );
 
-        return order_histories;
+    return order_histories;
+  } catch (error) {
+    console.error('Error getting all order histories');
+    throw error;
+  }
+}
 
-    } catch (error) {
-        console.error("Error getting all order histories");
-        throw error;
-    }
-};
+async function updateOrderHistory({ id, ...fields }) {
+  const { update } = fields;
 
-async function updateOrderHistory ({id, ...fields}) {
-    const { update } = fields;
-
-    const setString = Object.keys(fields)
+  const setString = Object.keys(fields)
     .map((key, index) => `"${key}"=$${index + 1}`)
-    .join(", ");
+    .join(', ');
 
-    try {
-        if(setString.length > 0){
-            await client.query(`
-            UPDATE order_history
-            SET ${setString}
-            WHERE id=${id}
-            RETURNING *;
-            `, Object.values(fields));
-        }
-        if(update === undefined){
-            return await getOrderHistoryById(id);
-        }
-    } catch (error) {
-        console.error('Error updating order history');
-        throw error;
+  try {
+    if (setString.length > 0) {
+      await client.query(
+        `
+        UPDATE order_history
+        SET ${setString}
+        WHERE id=${id}
+        RETURNING *;`,
+        Object.values(fields)
+      );
     }
-};
-
-async function deleteOrderHistoryById (id) {
-    try {
-        const { rows: deletedOrderHistory } = await client.query(`
-            DELETE FROM order_history
-            WHERE id=$1
-            RETURNING *;
-        `, [id]);
-
-        return deletedOrderHistory;
-
-    } catch (error) {
-        console.error('Error deleting order history');
-        throw error;
+    if (update === undefined) {
+      return await getOrderHistoryById(id);
     }
-};
+  } catch (error) {
+    console.error('Error updating order history');
+    throw error;
+  }
+}
+
+async function deleteOrderHistoryById(id) {
+  try {
+    const { rows: deletedOrderHistory } = await client.query(
+      `
+        DELETE FROM order_history
+        WHERE id=$1
+        RETURNING *;`,
+      [id]
+    );
+
+    return deletedOrderHistory;
+  } catch (error) {
+    console.error('Error deleting order history');
+    throw error;
+  }
+}
+
+async function deleteOrderHistoriesByUserId(userId) {
+  try {
+    const orders = await getOrderHistoryByUserId(userId);
+
+    if (orders && orders.length) {
+      //Delete the order detail dependencies
+      orders.forEach(order => {
+        order.orderDetails = deleteOrderDetailsByOrderId(order.id);
+        return order;
+      });
+
+      await client.query(`
+        DELETE FROM order_history
+        WHERE "userId"=${userId}
+        RETURNING *;`);
+
+      return orders;
+    }
+  } catch (error) {
+    console.error('Error deleting order history by user id');
+    throw error;
+  }
+}
 
 module.exports = {
-    createOrderHistory,
-    getOrderHistoryByUserId,
-    getAllOrderHistories,
-    updateOrderHistory,
-    deleteOrderHistoryById
+  createOrderHistory,
+  getOrderHistoryByUserId,
+  getAllOrderHistories,
+  updateOrderHistory,
+  deleteOrderHistoryById,
+  deleteOrderHistoriesByUserId,
 };
--- db/tables/product_reviews.js
+++ db/tables/product_reviews.js
@@ -1,30 +1,35 @@
 const client = require('../client');
 
-async function getProductReviewById(productReviewId){
+async function getProductReviewById(productReviewId) {
   try {
-    const {rows: [productReview] } = await client.query(`
+    const {
+      rows: [productReview],
+    } = await client.query(
+      `
     SELECT *
     FROM product_reviews
     WHERE id=$1;
-    `, [productReviewId]);
+    `,
+      [productReviewId]
+    );
 
     return productReview;
-
   } catch (error) {
     console.error('Error getting product review by id');
     throw error;
   }
 }
 async function createProductReview(productReview) {
-
   try {
-
     const columnNames = Object.keys(productReview).join('", "');
-      const valueString = Object.keys(productReview)
-        .map((key, index) => `$${index + 1}`)
-        .join();
+    const valueString = Object.keys(productReview)
+      .map((key, index) => `$${index + 1}`)
+      .join();
 
-    const {rows: [newProductReview] } = await client.query(`
+    const {
+      rows: [newProductReview],
+    } = await client.query(
+      `
         INSERT INTO product_reviews("${columnNames}")
         VALUES (${valueString})
         RETURNING *;`,
@@ -38,89 +43,111 @@ async function createProductReview(productReview) {
   }
 }
 
-async function getProductReviewsByProductId(productId){
-    try {
-        const {rows} = await client.query(`
+async function getProductReviewsByProductId(productId) {
+  try {
+    const { rows } = await client.query(
+      `
         SELECT *
         FROM product_reviews
         WHERE "productId"=$1;
-        `, [productId]);
+        `,
+      [productId]
+    );
 
-        rows.forEach((row) => {
-          delete row.productId
-        });
+    rows.forEach(row => {
+      delete row.productId;
+    });
 
-        return rows;
+    return rows;
+  } catch (error) {
+    console.error("Error getting product review by 'productId'");
+    throw error;
+  }
+}
 
-    } catch (error) {
-        console.error("Error getting product review by 'productId'");
-        throw error;
-    }
-};
-
-async function getProductReviewsByUserId(userId){
+async function getProductReviewsByUserId(userId) {
   try {
-      const {rows} = await client.query(`
+    const { rows } = await client.query(
+      `
       SELECT *
       FROM product_reviews
       WHERE "userId"=$1;
-      `, [userId]);
+      `,
+      [userId]
+    );
 
-      return rows;
-
+    return rows;
   } catch (error) {
-      console.error("Error getting product review by 'userId'");
-      throw error;
+    console.error("Error getting product review by 'userId'");
+    throw error;
   }
-};
+}
 
-async function updateProductReview({id, ...productReview}){
-
-    const setString = Object.keys(productReview)
+async function updateProductReview({ id, ...productReview }) {
+  const setString = Object.keys(productReview)
     .map((key, index) => `"${key}"=$${index + 1}`)
-    .join(", ");
+    .join(', ');
 
-    try {
-        if(setString.length > 0){
-            await client.query(`
+  try {
+    if (setString.length > 0) {
+      await client.query(
+        `
             UPDATE product_reviews
             SET ${setString}
             WHERE id=${id}
             RETURNING *;
-            `, Object.values(productReview));
-        }
-        if(productReview === undefined){
-            return await getProductReviewById(id);
-        }
-        
-    } catch (error) {
-        console.error("Error updating product review");
-        throw error;
+            `,
+        Object.values(productReview)
+      );
     }
-};
+    if (productReview === undefined) {
+      return await getProductReviewById(id);
+    }
+  } catch (error) {
+    console.error('Error updating product review');
+    throw error;
+  }
+}
 
-async function deleteProductReviewById(id){
-    try {
-        const { rows: [deletedProductReview] } = await client.query(`
+async function deleteProductReviewById(id) {
+  try {
+    const {
+      rows: [deletedProductReview],
+    } = await client.query(
+      `
         DELETE FROM product_reviews
         WHERE id=$1
         RETURNING *;
         `,
       [id]
     );
-        return deletedProductReview;
-        
-    } catch (error) {
-        console.error("Error deleting product review");
-        throw error;
-    }
-};
+    return deletedProductReview;
+  } catch (error) {
+    console.error('Error deleting product review');
+    throw error;
+  }
+}
 
+async function deleteProductReviewsByUserId(userId) {
+  try {
+    const { rows: deletedProductReviews } = await client.query(
+      `
+      DELETE FROM product_reviews
+      WHERE "userId"=${userId}
+      RETURNING *;`
+    );
+    return deletedProductReviews;
+  } catch (error) {
+    console.error('Error deleting product review by user id');
+    throw error;
+  }
+}
 
 module.exports = {
-    createProductReview,
-    getProductReviewsByProductId,
-    getProductReviewsByUserId,
-    updateProductReview,
-    deleteProductReviewById
+  createProductReview,
+  getProductReviewsByProductId,
+  getProductReviewsByUserId,
+  updateProductReview,
+  deleteProductReviewById,
+  deleteProductReviewsByUserId,
 };

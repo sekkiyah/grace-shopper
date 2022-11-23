dc26a423f0a03b08c9fbda076edf784835f59774
sekkiyah <seth_hinshaw@hotmail.com>
2022-11-23 00:13:58 -0600

    updated API database calls with corrected database function parameters

--- api/categories.js
+++ api/categories.js
@@ -6,100 +6,102 @@ const {
   getAllCategories,
   getCategoryById,
   updateCategory,
-  deleteCategory
+  deleteCategory,
 } = require('../db/tables/categories');
-
-
 
 //GET /api/categories
 categoriesRouter.get('/', async (req, res, next) => {
   try {
-
     const allCategories = await getAllCategories();
 
-    res.send(allCategories)
-
+    res.send(allCategories);
   } catch (error) {
     res.send({
       name: 'Categories Error',
       message: `API Unable to get All Categories`,
-      });
+    });
     next(error);
   }
 });
 
 //GET /api/categories/:categoryId
 
-promoCodesRouter.get('/:categoryId', /**requireAdmin, */ async (req, res, send) => {
-  const { categoryId } = req.params;
-  try {
-  const category = await getCategoryById(categoryId);
+promoCodesRouter.get(
+  '/:categoryId',
+  /**requireAdmin, */ async (req, res, send) => {
+    const { categoryId } = req.params;
+    try {
+      const category = await getCategoryById(categoryId);
 
-    res.send(category);
-
-  } catch (error) {
-    res.send({
-      name: 'Categories Error',
-      message: `Unable to get Category By ID`,
+      res.send(category);
+    } catch (error) {
+      res.send({
+        name: 'Categories Error',
+        message: `Unable to get Category By ID`,
       });
-    next(error);
+      next(error);
+    }
   }
-});
+);
 
 //POST /api/categories
-categoriesRouter.post('/', /*requireAdmin,*/ async (req, res, next) => {
-  const { name } = req.body;
-  
-  try {
-    
-    const category = await createCategory(name);
-  
-    res.send(category)
-  
-  } catch (error) {
-    res.send({
-      name: 'Categories Error',
-      message: `API Unable to create new Categories`,
+categoriesRouter.post(
+  '/',
+  /*requireAdmin,*/ async (req, res, next) => {
+    const { name } = req.body;
+
+    try {
+      const category = await createCategory({ name });
+
+      res.send(category);
+    } catch (error) {
+      res.send({
+        name: 'Categories Error',
+        message: `API Unable to create new Categories`,
       });
-    next(error);
+      next(error);
+    }
   }
-});
+);
 
 //PATCH /api/categories/categoryId
-categoriesRouter.patch('/:categoryId', /*requireAdmin,*/ async (req, res, next) => {
-  const { categoryId } = req.params;
-  const { name } = req.body;
+categoriesRouter.patch(
+  '/:categoryId',
+  /*requireAdmin,*/ async (req, res, next) => {
+    const { categoryId } = req.params;
+    const { name } = req.body;
 
-  try {
-    const updatedCategory = await updateCategory(categoryId, name);
+    try {
+      const updatedCategory = await updateCategory({ id: categoryId, name });
 
-    res.send(updatedCategory)
-
-  } catch (error) {
-    res.send({
-      name: 'Categories Error',
-      message: `API Unable to update Category`,
+      res.send(updatedCategory);
+    } catch (error) {
+      res.send({
+        name: 'Categories Error',
+        message: `API Unable to update Category`,
       });
-    next(error);
+      next(error);
+    }
   }
-});
+);
 
 //DELETE /api/category/categoryId
-categoriesRouter.delete('/:categoryId', /*requireAdmin,*/ async (req, res, next) => {
-  const { categoryId } = req.params;
-  try {
+categoriesRouter.delete(
+  '/:categoryId',
+  /*requireAdmin,*/ async (req, res, next) => {
+    const { categoryId } = req.params;
+    try {
+      const deletedCategory = await deleteCategory(categoryId);
 
-    const deletedCategory = await deleteCategory(categoryId)
-
-      res.send(deletedCategory)
-
-  } catch (error) {
-    res.send({
-      name: 'Categories Error',
-      message: `API Unable to delete Category`,
+      res.send(deletedCategory);
+    } catch (error) {
+      res.send({
+        name: 'Categories Error',
+        message: `API Unable to delete Category`,
       });
-    next(error);
+      next(error);
+    }
   }
-});
+);
 
 module.exports = categoriesRouter;

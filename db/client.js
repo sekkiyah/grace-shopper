fbdb76f5a72560cdb0a85d963589fe91d14abb4a
sekkiyah <seth_hinshaw@hotmail.com>
2022-11-22 23:39:43 -0600

    updated database seeding and connection

--- db/client.js
+++ db/client.js
@@ -1,14 +1,11 @@
 const { Pool } = require('pg');
+// require('dotenv').config(); //Seth will update prod db connection when needed
 
-const connectionString =
-  process.env.DATABASE_URL || 'https://localhost:5432/grace-shopper';
+const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/grace-shopper';
 
 const client = new Pool({
   connectionString,
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? { rejectUnauthorized: false }
-      : undefined,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
 
 module.exports = client;

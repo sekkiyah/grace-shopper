923dc0b18b9557d4c2b56c6af1ba3427b2c258fd
sekkiyah <seth_hinshaw@hotmail.com>
2022-11-22 22:22:02 -0600

    fixed categories database query issues

--- db/tables/categories.js
+++ db/tables/categories.js
@@ -1,31 +1,29 @@
 const client = require('../client');
 
-async function createCategory(name) {
-    try {
-        const { rows: [category] } = await client.query(`
-            INSERT INTO categories(name)
-            VALUES ($1)
-            ON CONFLICT (name) DO NOTHING
-            RETURNING *;
-            `, [name]);
+async function createCategory({ name }) {
+  try {
+    const {
+      rows: [category],
+    } = await client.query(`
+      INSERT INTO categories(name)
+      VALUES '${name}'
+      ON CONFLICT (name) DO NOTHING
+      RETURNING *;`);
 
-            return category;
-
-    } catch (error) {
-        console.error('Error creating category');
-        throw error;
-    }
-};
+    return category;
+  } catch (error) {
+    console.error('Error creating category');
+    throw error;
+  }
+}
 
 async function getAllCategories() {
   try {
     const { rows: categories } = await client.query(`
-    SELECT *
-    FROM categories;
-    `);
+      SELECT *
+      FROM categories;`);
 
     return categories;
-
   } catch (error) {
     console.error('Error getting all categories');
     throw error;
@@ -36,40 +34,33 @@ async function getCategoryById(id) {
   try {
     const {
       rows: [category],
-    } = await client.query(
-      `
-        SELECT *
-        FROM category
-        WHERE id=$1;
-        `,
-      [id]
-    );
+    } = await client.query(`
+      SELECT *
+      FROM category
+      WHERE id=${id};`);
 
-        return category;
-    } catch (error) {
-        console.error("Error getting category by id");
-        throw error;
-    }
-};
+    return category;
+  } catch (error) {
+    console.error('Error getting category by id');
+    throw error;
+  }
+}
 
 async function updateCategory(id, newName) {
   if (newName === undefined) {
-    console.error('New name not given')
-    return
+    console.error('New name not given');
+    return;
   }
   try {
     const {
       rows: [category],
-    } = await client.query(
-        `
-            UPDATE categories
-            SET name='${newName}'
-            WHERE id=${id}
-            RETURNING *;
-            `,);
+    } = await client.query(`
+      UPDATE categories
+      SET name='${newName}'
+      WHERE id=${id}
+      RETURNING *;`);
 
-            return category
-    
+    return category;
   } catch (error) {
     console.error('Error updating category');
     throw error;
@@ -78,25 +69,24 @@ async function updateCategory(id, newName) {
 
 async function deleteCategory(id) {
   try {
-    const { rows: [deletedCategory] } = await client.query(
-      `
-            DELETE FROM categories
-            WHERE id=${id}
-            RETURNING *;`,);
+    const {
+      rows: [deletedCategory],
+    } = await client.query(`
+      DELETE FROM categories
+      WHERE id=${id}
+      RETURNING *;`);
 
     return deletedCategory;
-    
   } catch (error) {
     console.error('Error deleting category');
     throw error;
   }
 }
 
-
 module.exports = {
   createCategory,
   getCategoryById,
   updateCategory,
   deleteCategory,
-  getAllCategories
+  getAllCategories,
 };

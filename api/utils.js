99a488e8169a864a694e6d0cc3f2567f16aa3a46
sekkiyah <seth_hinshaw@hotmail.com>
2022-11-23 21:13:40 -0600

    built out requireUser and requireAdmin utility functions

--- api/utils.js
+++ api/utils.js
@@ -1,15 +1,41 @@
+const { UnauthorizedError, UnauthorizedUserError } = require('./errors');
+const jwt = require('jsonwebtoken');
+
 function requireUser(req, res, next) {
   if (!req.user) {
     res.status(401).send({
-      error: 'Unauthorized user',
-      name: 'UnauthorizedUserError',
-      message: 'Unauthorized user',
+      error: 'Unauthorized',
+      name: 'UnauthorizedError',
+      message: UnauthorizedError(),
+    });
+  } else {
+    next();
+  }
+}
+
+function requireAdmin(req, res, next) {
+  if (!req.user) {
+    res.status(401).send({
+      error: 'Unauthorized',
+      name: 'UnauthorizedError',
+      message: UnauthorizedError(),
     });
   }
 
-  next();
+  const { role } = jwt.verify(req.token, process.env.JWT_SECRET);
+
+  if (role !== 'admin') {
+    res.status(401).send({
+      error: 'Unauthorized user',
+      name: 'UnauthorizedUserError',
+      message: UnauthorizedUserError(req.user.username),
+    });
+  } else {
+    next();
+  }
 }
 
 module.exports = {
   requireUser,
+  requireAdmin,
 };

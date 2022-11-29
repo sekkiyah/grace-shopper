00be4f1c13fa78799a75d0ce72007a41a71f8d2f
RachaelRoyack <rachaelroyack@aol.com>
2022-11-29 15:59:24 -0500

    added usercart api endpoints

--- api/users.js
+++ api/users.js
@@ -8,9 +8,16 @@ const {
   updateUserById,
   getUserById,
   deleteUserById,
+  addItemToUserCart,
+  getUserCartByUserId,
+  deleteUserCartByUserId,
+  deleteProductFromCart,
+  updateProductQuantityInCart,
+
 } = require('../db/tables');
 const { requireUser, requireAdmin } = require('./utils');
 const { UsernameTakenError, EmailTakenError } = require('./errors');
+
 // const { deleteUser } = require('../src/api');
 
 router.post('/register', async (req, res, next) => {
@@ -143,5 +150,70 @@ router.delete(
     }
   }
 );
+
+//USER CART ENDPOINTS:
+
+// //GET user's cart - DO NOT NEED THIS IF GETTING USER RETURNS OBJECT THAT INCLUDES CART
+// router.get( '/usersCart/:userId', /* requireUser, */ async (req, res, next) => {
+//     try {
+//       const { userId } = req.params;
+//       const userCart = await getUserCartByUserId(userId);
+//         res.send(userCart);
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
+
+
+// PATCH to add product to users cart
+router.patch('/usersCart/AddProduct/:userId', requireUser, async (req, res, next) => {
+  const { userId } = req.params;
+  const { productId, quantity } = req.body
+  try {
+      
+      const result = await addItemToUserCart(userId, productId, quantity);
+      res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+// PATCH to update product quantity in users cart
+router.patch('/usersCart/UpdateCart/:userId', requireUser, async (req, res, next) => {
+  const { userId } = req.params;
+  const { productId, quantity} = req.body
+  try {
+      const result = await updateProductQuantityInCart(userId, productId, quantity);
+      res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PATCH to delete product in users cart
+router.patch('/usersCart/DeleteProduct/:userId', requireUser, async (req, res, next) => {
+  const { userId } = req.params;
+  const { productId } = req.body
+  try {
+      const result = await deleteProductFromCart(userId, productId);
+      res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+// DELETE to delete usersCart - Check to see if this will be handled in order history
+router.patch('/usersCart/:userId', requireUser, async (req, res, next) => {
+  const { userId } = req.params;
+  try {
+      const result = await deleteUserCartByUserId(userId);
+      res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;

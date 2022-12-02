9ab7e76c98c2795aa9e0b1a306689aafb8584f55
sekkiyah <seth_hinshaw@hotmail.com>
2022-12-01 20:17:14 -0600

    removed unused code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,37 +27,6 @@ const NavBar = ({ logout, token, navigate }) => {
           </Nav>
         </Container>
       </Navbar>
-      {/* <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="/">Occult Outlet</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><Link to="/">Home</Link></li>
-                    <li><Link to="/products">Products</Link></li>
-                    <li><Link to="/cart">Cart</Link></li>
-                    {
-                        token ? (
-                            <>
-                                <li><Link to="/profile">Profile</Link></li>
-                                <li><Link to="/" onClick={() => logout()}><button class="btn btn-danger navbar-btn">Logout</button></Link></li>
-                            </>
-                        ) : (
-                            <>
-                                <li><Link to="/register">New Account</Link></li>
-                                <li><Link to="/login"><button class="btn btn-danger navbar-btn">Login</button></Link></li>
-                            </>
-                        )
-                    }
-                </ul>
-                <form class="navbar-form navbar-left" action="/action_page.php">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search"/>
-                    </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
-            </div>
-        </nav> */}
     </>
   );
 };

e24e9408901b55621fc1908e57b2c7d69cbc9511
Francis Harbourt <nharbourt@gmail.com>
2022-12-01 21:19:23 -0500

    created folder for admin with basic structure

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
   ProductDetails,
   Products,
   Checkout,
+  Admin
 } from './pages';
 import { Navbar } from './components';
 import {getUserInfo} from './api'
@@ -62,6 +63,7 @@ const App = () => {
         <Route path='/products/edit-product/:productId' element={<EditProduct token={token} navigate={navigate} />} />
         <Route path='/cart' element={<UserCart token={token} user={user} setUser={setUser}/>} />
         <Route path='/checkout' element={<Checkout />} />
+        <Route path='/admin' element={<Admin />} />
 
       </Routes>
     </>

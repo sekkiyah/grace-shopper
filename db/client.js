ccb914ac7bd90b11b41711cf314bc84353d07786
sekkiyah <seth_hinshaw@hotmail.com>
2022-12-03 13:10:55 -0600

    updated db connection config

--- db/client.js
+++ db/client.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
-require('dotenv').config(); 
+require('dotenv').config();
 
 const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/grace-shopper';
 
 const client = new Pool({
+  idleTimeoutMillis: 0,
   connectionString,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
+
+client.on('error', (err, client) => {
+  console.error('Unexpected error on client', err);
+  process.exit(-1);
 });
 
 module.exports = client;

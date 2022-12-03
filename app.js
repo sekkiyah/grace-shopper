f115c223333e464df5a5f8afeb607db05ddfc3c3
sekkiyah <seth_hinshaw@hotmail.com>
2022-12-03 14:29:10 -0600

    db connection timeout bug fixing

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-// require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 const router = require('./api');
-const client = require('./db/client');
-
-// client.connect((err, client, done) => {
-//   if (err) throw err;
-// });
 
 app.use(cors());
 app.use(morgan('dev'));

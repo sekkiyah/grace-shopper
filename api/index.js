8cc36a665768dc1f661933f9c46c613acad1403e
sekkiyah <seth_hinshaw@hotmail.com>
2022-12-03 10:18:10 -0600

    added heartbeat endpoint

--- api/index.js
+++ api/index.js
@@ -55,6 +55,10 @@ router.use('/order_history', orderHistoryRouter);
 const promoCodesRouter = require('./promo_codes');
 router.use('/promo_codes', promoCodesRouter);
 
+router.get('/health', async (req, res, next) => {
+  res.send('API server is running');
+});
+
 router.use('*', (req, res, next) => {
   res.status(404).send({
     name: 'Page Not Found',

b2268687f3692cb4ade8a7791042fef4fabb7dc4
Francis Harbourt <nharbourt@gmail.com>
2022-12-06 22:12:00 -0500

    added URLS

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:3000/api'
+// const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = 'https://occult-outlet-api.onrender.com/api';
 
 const createHeaders = token => {
   return token

643afc4250a75783a5cc59bb5a2132b3e970e0ad
alexmenke <alexrm2017@gmail.com>
2022-12-06 18:55:04 -0600

    added profile component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Welcome to Occult Outlet!</div>;
+  return (
+    <div>
+      <br></br>
+      <h1>Welcome to Occult Outlet!</h1>
+      <br></br>
+    </div>
+  )
 };
 
 export default Home;
